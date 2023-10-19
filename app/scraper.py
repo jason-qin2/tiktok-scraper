ba61e41b45bc9fd2a53ff36fd36770866ca5c1d9
jason-qin2 <qinzechen@hotmail.com>
2023-10-19 17:27:09 -0400

    Init tiktok scraper

---
+++ app/scraper.py
@@ -0,0 +1,53 @@
+from tiktokapipy.api import TikTokAPI
+from tiktokapipy import models
+from io import StringIO
+import csv
+
+def get_video_stats():
+    with TikTokAPI() as api:
+        data = []
+        tags = ["streetwear", "fashion", "styling", "outfit", "ootd"]
+        count = 0
+        for tag in tags:
+            #print(tag)
+            challenge = api.challenge(tag, video_limit=20)
+            for video in challenge.videos:
+                #print(count)
+                count += 1
+                vid = api.video(video.id)
+                creator = vid.creator()
+
+                try:
+                    video.comments._fetch_sync()
+                    comments = list(map(lambda x: x.text, video.comments._collected_values))
+                except:
+                    comments = []
+                
+                try:
+                    hashtags = list(map(lambda x: "#" + x.title, video.challenges))
+                except:
+                    hashtags = []
+                
+                # URL, account, views, likes, comments, saved, caption, hashtags, date posted, date collected
+                rowData = [
+                    models.video.video_link(video.id),
+                    creator.nickname,
+                    video.stats.play_count,
+                    video.stats.digg_count,
+                    video.stats.comment_count,
+                    comments,
+                    video.stats.share_count,
+                    video.desc,
+                    hashtags,
+                    video.create_time
+                ]
+                data.append(rowData)
+        #Produce output to be written into CSV
+        output = StringIO()
+        writer = csv.writer(output)
+        headers = ["Post URL", "Account", "Views", "Likes", "# of Comments", "Comments", "Saved", "Caption", "Hashtags", "Date Posted", "Date Collected"]
+        writer.writerow(headers)
+        writer.writerows(data)
+        print(output.getvalue())
+
+get_video_stats()
